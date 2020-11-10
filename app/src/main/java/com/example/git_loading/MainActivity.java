0f866b82d92febedf3b3cfcab5f99e593b450db3
unknown <vlad.rodionov.02.02@mail.ru>
2020-11-10 13:14:01 +0400

    Залил после ввода данных

--- app/src/main/java/com/example/git_loading/MainActivity.java
+++ app/src/main/java/com/example/git_loading/MainActivity.java
@@ -6,17 +6,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText telephoneInput;
+    public EditText passwordInput;
+    public TextView textBlock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        telephoneInput = (EditText) findViewById(R.id.phone);
+        passwordInput = (EditText) findViewById(R.id.password);
+        textBlock = (TextView) findViewById(R.id.textBlock);
     }
 
+
     public void onClick(View view) {
-        Intent intent = new Intent(MainActivity.this, perehod.class);
-        startActivity(intent);
+        if (telephoneInput.getText().toString().equals("") && passwordInput.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(), "Какая-то из строк не заполнена!", Toast.LENGTH_SHORT).show();
+        } else {
+
+            Intent intent = new Intent(MainActivity.this, perehod.class);
+            startActivity(intent);
+        }
     }
 }
